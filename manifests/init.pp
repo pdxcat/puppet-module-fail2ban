73c3d3e8e485fc3ab1f83e79a7219b6c104175ba
Brianna Buckner <bucknerb@cat.pdx.edu>
2012-04-24 15:29:20 -0700

    Add logic

---
+++ manifests/init.pp
@@ -0,0 +1,30 @@
+#
+#
+#
+
+class fail2ban (
+  $config_file = "puppet://$server/modules/fail2ban/fail2ban.conf",
+  $jail_file = "puppet://$server/modules/fail2ban/jail.conf"
+) {
+  package { 'fail2ban': ensure => installed }
+
+  service { 'fail2ban':
+    ensure => running,
+    require => Package['fail2ban'],
+  }
+
+  file { '/etc/fail2ban/fail2ban.conf':
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    source => $config_file,
+  }
+
+  file { '/etc/fail2ban/jail.conf':
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    source => $jail_file,
+  }
+
+}
